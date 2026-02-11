7f5192b3d8ae5a337a5943f76de7a521ee53cf9c
liuzhendong <liuzhendong@lingchuang-dl.com>
2026-02-11 17:44:41 +0800

    [test] kotlin测试dropLastWhile 用法

--- app/src/main/java/com/zd/featuredemo/Test.kt
+++ app/src/main/java/com/zd/featuredemo/Test.kt
@@ -4,6 +4,14 @@ object Test {
 
     @JvmStatic
     fun main(args: Array<String>) {
+        val requestString = "requestId|true|0|1"
+        val requestString2 = "requestId|true|0|1||"
+
+        val split = requestString.split("|")
+        val split2 = requestString2.split("|")
+        val result = split.drop(1).dropLastWhile { it.isEmpty() }
+        val result2 = split2.drop(1).dropLastWhile { it.isEmpty() }
+
 
     }
 }
